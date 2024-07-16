ae7f0128c59d02139351bc5f9b4e2682bbd147f6
Marquis Abah <marquis.abah@shuttlers.ng>
2024-07-16 20:36:25 +0100

    updated name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default {
   target: "static",
   app: {
     head: {
-      title: "National Profile Admin",
+      title: "CodeRed | Home",
       htmlAttrs: { lang: "en" },
       meta: [
         { charset: "utf-8" },
